d6523d9f1be5c4ecdafa06413ab0e3fdf2c9f69c
Hưởng Nguyễn Văn <huongnguyenvan@Huongs-MacBook-Air.local>
2020-12-07 10:06:46 +0700

    add WebsecurityConfig

---
+++ sms/src/main/java/com/shopnow/WebSecurityConfig.java
@@ -0,0 +1,56 @@
+package com.nauthui7.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
+
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private DataSource dataSource;
+    @Bean
+    public BCryptPasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+        //check login
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery("SELECT username, password, enable FROM users WHERE username = ?;")
+                .authoritiesByUsernameQuery("SELECT username ,concat('ROLE_',role) as roles FROM users WHERE username = ?;")
+                .passwordEncoder(encoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/api/**").permitAll()
+                .and()
+                .authorizeRequests().antMatchers("/**").permitAll()
+                .and()
+//                .authorizeRequests().antMatchers("/blog").hasRole("USER")
+//                .and()
+//                .authorizeRequests().antMatchers("/tag").hasRole("USER")
+//                .and()
+                .formLogin()
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+        http.authorizeRequests()
+                .and().formLogin()
+                .defaultSuccessUrl("/blog")
+                .failureUrl("/")
+                .and().logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login");
+        http.csrf().disable();
+    }
+}
