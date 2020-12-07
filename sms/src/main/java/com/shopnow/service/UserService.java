eca78ab9c1eeee21d93733c71ae1a0652e821354
Hưởng Nguyễn Văn <huongnguyenvan@Huongs-MacBook-Air.local>
2020-12-07 10:58:15 +0700

    updated

--- sms/src/main/java/com/shopnow/service/UserService.java
+++ sms/src/main/java/com/shopnow/service/UserService.java
@@ -3,7 +3,6 @@ package com.shopnow.service;
 import com.shopnow.model.User;
 import org.springframework.stereotype.Service;
 
-@Service
 public interface UserService extends BaseService<User>{
     User findByUsername(String username);
 }
