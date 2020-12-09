f0f86ea47985500349f1df5183413ce52db47ea7
Do Chanh Tin <@gmail>
2020-12-09 14:38:22 +0700

    add controller

--- sms/src/main/java/com/shopnow/controller/admin/CustomerController.java
+++ sms/src/main/java/com/shopnow/controller/admin/CustomerController.java
@@ -1,4 +1,14 @@
 package com.shopnow.controller.admin;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/customer")
 public class CustomerController {
+    @GetMapping
+    public String index(){
+        return "admin/customer";
+    }
 }
