5a1b4e517ee68824dd9aac1f3f42959f026e6601
nauthui2018 <61721233+nauthui2018@users.noreply.github.com>
2020-12-19 11:07:20 +0700

    updated type of creating_datein customer_group

--- sms/src/main/java/com/shopnow/model/CustomerGroup.java
+++ sms/src/main/java/com/shopnow/model/CustomerGroup.java
@@ -19,8 +19,9 @@ public class CustomerGroup {
     private Long id;
     private String name;
 
-    @JsonFormat(pattern="dd/MM/yyyy HH:mm:ss")
-    private ZonedDateTime creating_date = ZonedDateTime.now();
+//    @JsonFormat(pattern="dd/MM/yyyy HH:mm:ss")
+//    private ZonedDateTime creating_date = ZonedDateTime.now();
+    private String creating_date;
     private boolean deleted = false;
 
     @OneToMany(mappedBy = "customer_group")
